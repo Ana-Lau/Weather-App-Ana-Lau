174a20fae619c7ccecc4fd58bbf85018ad38ddda
Ana Lau <ana.lau.gouveia@gmail.com>
2020-12-14 19:51:30 +0000

    Updated forecast

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-function formatDate(date) {
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
   let hours = date.getHours();
   if (hours < 10) {
     hours = `0${hours}`;
@@ -7,7 +8,6 @@ function formatDate(date) {
   if (minutes < 10) {
     minutes = `0${minutes}`;
   }
-
   let dayIndex = date.getDay();
   let days = [
     "Sunday",
@@ -19,20 +19,27 @@ function formatDate(date) {
     "Saturday",
   ];
   let day = days[dayIndex];
-
   let currentDate =
     date.getDate() + "/" + (date.getMonth() + 1) + "/" + date.getFullYear();
-  return `${day} <br>
-  ${currentDate} ${hours}h${minutes}`;
+  return `${day} <br /> ${currentDate} ${hours}h${minutes}`;
 }
-
+function formatHours(timestamp) {
+  let time = new Date(timestamp);
+  let hours = time.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = time.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${hours}:${minutes}`;
+}
 let date = document.querySelector("#date");
 let currentTime = new Date();
 date.innerHTML = formatDate(currentTime);
-
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", handleSubmit);
-
 function displayWeatherCondition(response) {
   let iconElement = document.querySelector("#icon");
   let wind = Math.round(response.data.wind.speed);
@@ -43,7 +50,6 @@ function displayWeatherCondition(response) {
   document.querySelector(
     "#humidity"
   ).innerHTML = `Humidity: ${response.data.main.humidity}%`;
-
   document.querySelector("#wind").innerHTML = `Wind: ${wind} km`;
   iconElement.setAttribute(
     "src",
@@ -51,53 +57,41 @@ function displayWeatherCondition(response) {
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
-
 function displayForecast(response) {
-  let forecastElement = document.querySelectorAll("#forecast");
+  let forecastElement = document.querySelector("#forecast");
   forecastElement.innerHTML = null;
   let forecast = null;
-
   for (let index = 0; index < 6; index++) {
     forecast = response.data.list[index];
-    forecastElement.innerHTML = ` 
-  
-          <div class="col">
-            
-              ${getHours(forecast.dt)}
+    forecastElement.innerHTML += ` 
+          <div class="col-2 text-center">
+              ${formatHours(forecast.dt * 1000)}
               <br />
-              ${Math.round(forecast.main.temp_min)}° 
-              <strong>
-              ${Math.round(forecast.main.temp_max)}°</strong>
- 
-              <img scr="http://openweathermap.org/img/wn/${
+              ${Math.round(forecast.main.temp_min)}° <strong> ${Math.round(
+      forecast.main.temp_max
+    )}°</strong>
+              <img src="http://openweathermap.org/img/wn/${
                 forecast.weather[0].icon
               }@2x.png"
               />
-           
           </div>
-        
   `;
   }
 }
-
 function searchCity(city) {
   let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayWeatherCondition);
-
   apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
-
 function handleSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-text-input");
   let h1 = document.querySelector("h1");
-
   h1.innerHTML = `${searchInput.value}`;
   searchCity(searchInput.value);
 }
-
 function searchLocation(position) {
   let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
@@ -105,25 +99,20 @@ function searchLocation(position) {
   apiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
-
 function getCurrentLocation(event) {
   event.preventDefault();
   navigator.geolocation.getCurrentPosition(searchLocation);
 }
-
 function convertToFahrenheit(event) {
   event.preventDefault();
   let temperatureElement = document.querySelector("#temperature");
   temperatureElement.innerHTML = 66;
 }
-
 function convertToCelsius(event) {
   event.preventDefault();
   let temperatureElement = document.querySelector("#temperature");
   temperatureElement.innerHTML = 19;
 }
-
 let currentLocationButton = document.querySelector("#current-location-button");
 currentLocationButton.addEventListener("click", getCurrentLocation);
-
 searchCity("Lisbon");
